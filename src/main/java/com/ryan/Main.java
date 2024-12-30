22fd7914f03bf2b4e1371c4d70761144b098aef4
Ryan <ryan46.kim@gmail.com>
2024-12-30 20:09:30 +0900

    second commit

---
+++ src/main/java/com/ryan/Main.java
@@ -0,0 +1,10 @@
+package com.ryan;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("Test2");
+        System.out.println("Test3");
+
+    }
+}
